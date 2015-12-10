7e54d7453cfd4d0e04f1102752e376917afb7dfc
corentineuvrard <one.snsd@gmail.com>
2015-12-10 11:23:59 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -3,6 +3,23 @@ GameStates.Game = function (game) {
 	this.currentWeapon = 0;
 };
 
+var Enemy = function (game, key) {
+	
+	Phaser.Sprite.call(this, game, 0, 0, key);
+	
+	//this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+	
+	//this.anchor.set(0.5);
+	
+	this.checkWorldBounds = true;
+	this.outOfBoundsKill = true;
+	this.exists = false;
+	
+	this.tracking = false;
+	this.scaleSpeed = 0;
+	
+};
+
 var Bullet = function (game, key) {
 
 	Phaser.Sprite.call(this, game, 0, 0, key);
